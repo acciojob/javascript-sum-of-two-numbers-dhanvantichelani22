296e542163bf581931bbb0af56e613b76a716a00
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2024-01-09 20:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
 // Write your code here and print the output using alert function
+if(a!=undefined && b!=undefined)
+         document.getElementById('output').innerText = parseInt(a)+parseInt(b);
+	else
+		document.getElementById('output').innerText = "Invalid input. Please enter a valid number.";
